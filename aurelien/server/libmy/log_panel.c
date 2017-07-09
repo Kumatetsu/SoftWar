b8155509dce489e04b52ed0f42019b4bcab0414f
aurelien <aurelien@Debian>
2017-07-09 20:43:08 +0200

    [SERVER] Libmy add correction on my_strdup and logger

--- aurelien/server/libmy/log_panel.c
+++ aurelien/server/libmy/log_panel.c
@@ -9,29 +9,45 @@
 */
 
 #include <stdio.h>
+#include <time.h>
 #include "libmy.h"
+
+/*
+** https://stackoverflow.com/questions/1442116/how-to-get-date-and-time-value-in-c-program
+*/
+void	print_time(FILE *o)
+{
+  time_t t = time(NULL);
+  struct tm *tm = localtime(&t);
+  char s[64];
+  strftime(s, sizeof(s), "%c", tm);
+  fprintf(o, "%s ", s);
+}
 
 void    log_error(char *str, FILE *o)
 {
-  
+  print_time(o);
   fprintf(o, "< ERROR > ");
   fprintf(o, str);
 }
 
 void    log_warning(char *str, FILE *o)
 {
+  print_time(o);
   fprintf(o, "< WARNING > ");
   fprintf(o, str);
 }
 
 void    log_info(char *str, FILE *o)
 {
+  print_time(o);
   fprintf(o, "< INFO > ");
   fprintf(o, str);
 }
 
 void    log_debug(char *str, FILE *o)
 {
+  print_time(o);
   fprintf(o, "< DEBUG > ");
   fprintf(o, str);
 }
