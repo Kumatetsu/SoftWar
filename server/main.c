3b72f356826e9c8b79b9732104cbce1fadf52b71
aurelien <aurelien@Debian>
2017-07-12 15:46:06 +0200

    Ajout de la lib json-c, voir Pivotal pour le lien de la doc. delete des fichiers temp commités par négligence ^^

--- server/main.c
+++ server/main.c
@@ -11,10 +11,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <czmq.h>
+#include <json/json.h>
 #include "libmy.h"
 #include "argument.h"
 #include "Softwar_ctx.h"
 #include "Notification.h"
+#include "Game_info.h"
 #include "rep.h"
 #include "pub.h"
 #include "poll.h"
@@ -27,11 +29,42 @@ int		main(int argc, char *argv[])
   t_swctx	*ctx;
   char		*message = NULL;
   zsock_t	*publisher;
+  t_game_info	*game_info;
 
   if (sw_parse(argc, argv))
     my_log(__func__, "failed to parse SoftWar arguments", 2);
   ctx = get_swctx(); // à ce moment là, soit ctx est set par les arguments
 		     // soit on récupère ici la valeur par défaut.
+    /*
+  ** Serialization example:
+  */
+  char *str = "{'name':'joys of promagramming'}";
+  json_object *jobj = json_tokener_parse(str);
+  enum json_type type = json_object_get_type(jobj);
+
+  switch (type) {
+  case json_type_null: printf("json_type_nulln");
+    break;
+  case json_type_boolean: printf("json_type_booleann");
+    break;
+  case json_type_double: printf("json_type_doublen");
+    break;
+  case json_type_int: printf("json_type_intn");
+    break;
+  case json_type_object: printf("json_type_objectn");
+    break;
+  case json_type_array: printf("json_type_arrayn");
+    break;
+  case json_type_string: printf("json_type_stringn");
+    break;
+  }
+
+  /*
+  ** Implémentation d'une sérialisation sur la structure game info:
+  */
+  if ((game_info = init_game_info(5, 0, "Poney Content", 8)) == NULL)
+    return (1);
+  printf("\nStringified game_info: %s", game_info_to_json(game_info));
   /*
   ** REP/REQ server init
   ** voir /src/server/rep.c et pub.c
@@ -65,6 +98,7 @@ int		main(int argc, char *argv[])
     }
   /*
   ** libmy extended cleaning
+  ** options are cleaned in /src/tools/argument.c
   */
   delete_logger();
   /*
