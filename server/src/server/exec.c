794ede0c768377c987b16cc98fc30fa3390286a6
aurelienCastellarnau <castellarnau.a@gmail.com>
2017-08-06 23:39:42 +0200

    [serveur] ajout des fichiers de gestion de la boucle de jeu et de l'exécution des commandes.

---
+++ server/src/server/exec.c
@@ -0,0 +1,76 @@
+/*
+** exec.c for SoftWar in 
+** 
+** Made by CASTELLARNAU Aurelien
+** Login   <castel_a@etna-alternance.net>
+** 
+** Started on  Wed Aug  2 21:58:43 2017 CASTELLARNAU Aurelien
+** Last update Wed Aug  2 22:16:12 2017 CASTELLARNAU Aurelien
+*/
+
+#include "libmy.h"
+#include "hash.h"
+#include "Game_manager.h"
+#include "command.h"
+#include "exec.h"
+
+/*
+** Méthode très basique pour l'instant
+** possibilité d'ajouter une logique de validation
+*/
+char	**parse_input(char *input)
+{
+  char	**arr;
+
+  if ((arr = my_split(input, '|')) == NULL)
+    return (NULL);
+  if (arr[1] == NULL
+      && (arr[1] = my_strdup("no argument provide")) == NULL)
+    return (NULL);
+  return (arr);
+}
+
+/*
+** paramètres: le char * contenant la commande (identify|identity)
+**	       le t_game_manager (et donc la Game_info)
+**	       l'id du player.
+** fait appel aux méthode de hash.c pour récupérer la bonne
+** fonction dans commands et l'exécuter.
+*/
+char		*exec(	char *input,
+			t_game_manager **manager,
+			char *active_id)
+{
+  char		log[50];
+  int		hash;
+  t_command	**commands;
+  char		**inputs;
+  char		*output;
+
+  my_log(__func__, "start", 3);
+  commands = get_commands();
+  if ((inputs = parse_input(input)) == NULL)
+    return (NULL);
+  if ((hash = hash_command(inputs[0])) == HCOMMANDERR)
+    my_log(__func__, "retrieve hash error", 2);
+  if (hash != HCOMMANDUNKNOW)
+    {
+      sprintf(log, "execution of function %s", inputs[0]);
+      my_log(__func__, log, 3);
+      if ((output = commands[hash](manager, active_id, inputs[1])) == NULL)
+	{
+	  sprintf(log, "execution of function %s failed", inputs[0]);
+	  my_log(__func__, log, 2);
+	  if ((output = my_strdup("ko|server error")) == NULL)
+	    return (NULL);
+	  return (output);
+	}
+      sprintf(log, "execution of function %s succeeded, return = %s", inputs[0], output);
+      return (output);
+    }  
+  sprintf(log, "command %s doesn't exist", inputs[0]);
+  my_log(__func__, log, 2);
+  if ((output = my_strdup("ko|unknow command")) == NULL)
+    return (NULL);
+  return (output);
+}
---
+++ server/src/server/runtime.c
@@ -0,0 +1,98 @@
+/*
+** runtime.c for SoftWar in 
+** 
+** Made by CASTELLARNAU Aurelien
+** Login   <castel_a@etna-alternance.net>
+** 
+** Started on  Sun Jul 30 23:34:27 2017 CASTELLARNAU Aurelien
+** Last update Sun Jul 30 23:38:57 2017 CASTELLARNAU Aurelien
+*/
+
+#include <json/json.h>
+#include "libmy.h"
+#include "Game_manager.h"
+#include "Softwar_ctx.h"
+#include "Player.h"
+#include "command.h"
+#include "hash.h"
+#include "pub.h"
+#include "poll.h"
+#include "router.h"
+#include "exec.h"
+#include "runtime.h"
+
+int		init_network(t_swctx **ctx)
+{
+  t_swsock	*pub;
+  t_swsock	*router;
+
+  // pub/sub socket
+  pub = create_socket("pub", init_pub((*ctx)->pub_port));
+  if (add_link(&((*ctx)->sockets), pub))
+    return (1);
+  // req/router socket
+  router = create_socket("router", init_router((*ctx)->rep_port));
+  if (add_link(&((*ctx)->sockets), router))
+    return (1);
+  return (0);
+}
+
+/*
+** Boucle de jeu
+*/
+int		serve_game(t_swctx **ctx, t_game_manager **manager)
+{
+  char		log[80];
+  char		*ret;
+  zmsg_t	*response;
+  zframe_t	*address;
+  char		*input;
+  
+  while (!zsys_interrupted)
+    {
+      if ((response = init_poll(ctx)) == NULL)
+	return (1);
+      address = zmsg_pop(response);
+      if (((*ctx)->active_id = my_strdup(zmsg_popstr(response))) == NULL)
+	return (1);
+      input = zmsg_popstr(response);
+      sprintf(log, "active id: %s, input: %s", (*ctx)->active_id, input);
+      my_log(__func__, log, 3);
+      /*
+      ** La fonction exec() est chargée  de l'exécution des
+      ** commandes. C'est dans exec que ce fait le
+      ** traitement de l'input (parsing, hash,
+      ** modif de manager et donc de game info).
+      */
+      if ((ret = exec(input, manager, (*ctx)->active_id)) == NULL)
+	return (1);
+      sprintf(log, "return: %s", ret);
+      my_log(__func__, log, 3);
+      zmsg_pushstr(response, ret);
+      zmsg_pushstr(response, (*ctx)->active_id);
+      zmsg_push(response, address);
+      zmsg_send(&response, (*ctx)->active_socket->socket);
+      my_log(__func__, "zmsg sent", 3);
+    }
+  return (0);
+}
+		   
+int			init_runtime()
+{
+  t_swctx		*ctx;
+  t_game_manager	*manager;
+
+  ctx = get_swctx();
+  manager = get_game_manager();
+  init_network(&ctx);
+  if (serve_game(&ctx, &manager))
+    {
+      my_log(__func__, "serving game failed", 1);
+      return (1);
+    }
+  free_hashes();
+  manager->free(); // free game_info static structure
+  free(manager); // free the function pointer structure 'game_manager'
+  free_ctx();
+  return (0);
+}
