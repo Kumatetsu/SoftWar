a8860ddfa1070d04aae8528c89af78c39ed2ba90
kumatetsu <billaudjean@gmail.com>
2017-09-14 20:19:19 +0200

    voilà le thread devrait être complet !

--- server/src/server/thread.c
+++ server/src/server/thread.c
@@ -5,7 +5,7 @@
 ** Login   <billau_j@etna-alternance.net>
 ** 
 ** Started on  Thu Aug 17 17:00:01 2017 BILLAUD Jean
-** Last update Thu Sep 14 17:47:27 2017 BILLAUD Jean
+** Last update Thu Sep 14 20:10:18 2017 BILLAUD Jean
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,6 +17,7 @@
 #include "Game_manager.h"
 #include "Softwar_ctx.h"
 #include "thread_data.h"
+#include "Enum.h"
 
 void		refresh_ap(t_game_info **info)
 {
@@ -34,26 +35,53 @@ void		refresh_ap(t_game_info **info)
   return;
 }
 
+int		dead(t_chain *players)
+{
+  t_link	*l;
+  int		count;
+  
+  l = players->first;
+  while (l)
+    {
+      if (((t_player*)(l->content))->energy <= 0) {
+	count++;
+	l = l->next;
+	remove_link(&players, l->prev);
+      }
+    }
+  return (count);
+}
+
 void 		*tic_thread(void *manager)
 {
   t_thread	*thread = (t_thread *)(manager);
   zsock_t	*pub;
   json_object	*json;
   uint		cycle;
+  int		dead_count;
+  int		count;
 
   srand(time(NULL));
+  count = 0;
   cycle = thread->ctx->cycle;
   pub = ((t_swsock *)(thread->ctx->sockets->first->content))->socket;
+  zstr_sendf(pub, "%s %d", "Softwar", GAME_START);
   my_put_nbr(cycle);
   while (!zsys_interrupted) {
     usleep(cycle);
+    dead_count = dead(thread->info->players);
+    while(dead_count >= count) {
+      zstr_sendf(pub, "%s %d", "Softwar", CLIENT_DEAD);
+    }  
     json = game_info_to_json(thread->info);
-    zstr_sendf(pub, "%s %s", "Softwar", json_object_to_json_string(json));
+    zstr_sendf(pub, "%s %d %s", "Softwar", CYCLE,json_object_to_json_string(json));
     my_putstr(json_object_to_json_string(json));
     my_putstr("\n");
     energy_fall(thread->info->map_size);
     refresh_ap(&thread->info);
+    count = 0;
   }
+   zstr_sendf(pub, "%s %d", "Softwar", GAME_END);
   pthread_exit(NULL);
 }
 
