f96cc0c8adb8ac44d2da3dcab976524db45d6fa2
kumatetsu <billaudjean@gmail.com>
2017-10-01 11:21:43 +0200

    nettoyage et rangement des dossiers

--- clients_test/sub/client_sub.py
+++ clients_test/sub/client_sub.py
@@ -4,33 +4,12 @@ import json
 from pprint import pprint
 from graphics import *
 
-win = GraphWin('Softwar', 500, 600)
+win = GraphWin('Softwar', 400, 400)
 
 def clear(win):
     for item in win.items[:]:
         item.undraw()
     win.update()
-
-def clear_label(win, label):
-    label.undraw()
-    win.update()
-
-def write_notif(win, notif_type):
-    if notif_type == '0':
-        text = "Cycle info"
-    elif notif_type == '1':
-        text = "Game started"
-    elif notif_type == '2':
-        text = "Game finished"
-    elif notif_type == '3':
-        text = "Client dead"
-    elif notif_type == '4':
-        text = "Client Win"
-    else:
-        text = "Notif type does not exist"
-    label = Text(Point(100, 550), text)
-    label.draw(win)
-    return label
 
 def draw_client(win, x, y, identity, gap):
     client_x = x * gap - (gap / 2)
@@ -50,26 +29,29 @@ def draw_cell(win, x, y, value, gap):
     label = Text(Point((gap * x - gap / 2), (gap * y - gap / 2)), value)
     label.draw(win)
 
+#def my_json(win):
 def my_json(win, messagedata):
+    data = json.loads(messagedata)
+    #str = '{ "map_size": 5, "game_status": 0, "players": [{"identity": "0x00", "x": 1, "y": 1}, {"identity": "0x01", "x": 1, "y": 4}], "energy_cells": [{"x": 2, "y": 1, "value": 5}, {"x": 3, "y": 3, "value": 15}]}'
     data = json.loads(messagedata)
     map_size = data["map_size"]
     clear(win)
     gap = aff_map(win, map_size)
     game_status = data["game_status"]
-    energy_cells = data["energy_cells"]
-    if energy_cells:
-        for j in range(0, len(energy_cells)):
-            cell_x = energy_cells[j]["x"] + 1
-            cell_y = energy_cells[j]["y"] + 1
-            cell_value = energy_cells[j]["value"]
-            draw_cell(win, cell_x, cell_y, cell_value, gap)
     players = data["players"]
     if (players != None):
         for i in range(0, len(players)):
             pl_identity = players[i]["identity"]
-            pl_x = players[i]["x"] + 1
-            pl_y = players[i]["y"] + 1 
+            pl_x = players[i]["x"]
+            pl_y = players[i]["y"]
             draw_client(win, pl_x, pl_y, pl_identity, gap)
+    energy_cells = data["energy_cells"]
+    if energy_cells:
+        for j in range(0, len(energy_cells)):
+            cell_x = energy_cells[j]["x"]
+            cell_y = energy_cells[j]["y"]
+            cell_value = energy_cells[j]["value"]
+            draw_cell(win, cell_x, cell_y, cell_value, gap)
         
 def socket(win):
     port = "5556"
@@ -90,31 +72,26 @@ def socket(win):
     topicfilter = "Softwar"
     socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
 
-    label = None
-
-    while True:
+    total_value = 0
+    for update_nbr in range (5):
         string = socket.recv()
-        topic, notif_type, messagedata = string.split(" ", 2)
-        print notif_type
-        if notif_type == '0':
-            my_json(win, messagedata)
-        if label != None:
-            clear_label(win, label)
-        label = write_notif(win, notif_type)
+        topic, messagedata = string.split(" ", 1)
+        print(messagedata)
+        my_json(win, messagedata)
         
 def close(win):
     win.getMouse()
     win.close()
 
 def aff_map(win, map_size):
-    gap = 500 / map_size
+    gap = 400 / map_size
     x = gap
-    while (x <= 500):
+    while (x <= 400):
         pt = Point(x, 0)
-   	horizontal = Line(pt, Point(x,500))
+   	horizontal = Line(pt, Point(x, 400))
    	horizontal.draw(win)
         pt2 = Point(0, x)
-        vertical = Line(pt2, Point(500, x))
+        vertical = Line(pt2, Point(400, x))
         vertical.draw(win)
         x = x + gap
     return gap
