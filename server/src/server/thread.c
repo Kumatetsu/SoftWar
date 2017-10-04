5929eaacc8d7afbe63e0682ccd5ac7ce97dbf24b
kumatetsu <billaudjean@gmail.com>
2017-10-04 23:35:45 +0200

    on fixe le tout avec du huhu patafix, plus de segfault sur les dead people (normalement), il reste des fixes minimes à faire, pour l'envoi des bonns game info

--- server/src/server/thread.c
+++ server/src/server/thread.c
@@ -5,7 +5,7 @@
 ** Login   <billau_j@etna-alternance.net>
 ** 
 ** Started on  Thu Aug 17 17:00:01 2017 BILLAUD Jean
-** Last update Sat Sep 30 15:21:40 2017 BILLAUD Jean
+** Last update Wed Oct  4 23:16:40 2017 BILLAUD Jean
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -36,15 +36,27 @@ int		dead(t_chain *players)
 {
   t_link	*l;
   int		count;
+  char		log[50];
   
+  sprintf(log, "in dead");
+  my_log(__func__, log, 3);
   l = players->first;
   count = 0;
   while (l)
     {
-      if (((t_player*)(l->content))->energy <= 0) {
+      sprintf(log, "dans la boucle dead");
+      my_log(__func__, log, 3);
+      if (((t_player*)(l->content))->energy <= 0 || ((t_player*)(l->content))->energy >= 100 ) {
+	sprintf(log, "killing player");
+	my_log(__func__, log, 3);
 	count++;
-	l = l->next;
-	remove_link(&players, l->prev);
+	if (l->next != NULL) {
+	  l = l->next;
+	  remove_link(&players, l->prev);
+	} else {
+	  remove_link(&players, l);
+	  l = NULL;
+	}
       } else {
 	l = l->next;
       }
@@ -77,12 +89,9 @@ void 		*tic_thread(void *manager)
   json_object	*json;
   uint		cycle;
   int		dead_count;
-  int		count;
-  int		nb_player;
   char		output[1024];
 
   srand(time(NULL));
-  count = 0;
   cycle = thread->ctx->cycle;
   pub = ((t_swsock *)(thread->ctx->sockets->first->content))->socket;
   if (thread == NULL)
@@ -100,30 +109,34 @@ void 		*tic_thread(void *manager)
       usleep(cycle);
       undisabledme(&(thread->info));
       if (thread->info->game_status == 1)
-	zstr_sendf(pub, "%s %d", "Softwar", GAME_START);
+	zstr_sendf(pub, "%s %d %s", "Softwar", GAME_START, "{}");
       dead_count = dead(thread->info->players);
-      nb_player = (4 - dead_count);
-      while (dead_count > count)
+      if (dead_count == -2)
+	pthread_exit(NULL);
+      while (dead_count)
 	{
-	  zstr_sendf(pub, "%s %d", "Softwar", CLIENT_DEAD);
-	  count++;
+	  zstr_sendf(pub, "%s %d %s", "Softwar", CLIENT_DEAD, "{}");
 	  my_log(__func__, "client dead\n", 3);
+	  --dead_count;
 	}
-      if (nb_player < 2)
+      if (thread->info->players->index < 2 && thread->info->game_status)
 	{
-	  zstr_sendf(pub, "%s %d", "Softwar", CLIENT_WIN);
-	  zstr_sendf(pub, "%s %d", "Softwar", GAME_END);
+	  zstr_sendf(pub, "%s %d %s", "Softwar", CLIENT_WIN, "{}");
+	  zstr_sendf(pub, "%s %d %s", "Softwar", GAME_END, "{}");
 	  thread->info->game_status = 0;
 	  my_log(__func__, "client win, game is end\n", 3);
-	}
+	}  
       json = game_info_to_json(thread->info);
       sprintf(output, "%s %d %s", "Softwar", CYCLE, json_object_to_json_string(json));
       my_log(__func__, output, 5);
       zstr_sendf(pub, "%s %d %s", "Softwar", CYCLE, json_object_to_json_string(json));
-      if (energy_fall(&thread->info))
-	pthread_exit(NULL);
+      sprintf(output, "%d", thread->info->game_status);
+      my_log(__func__, output, 3);
+      if(thread->info->game_status == 1) {
+	if (energy_fall(&thread->info))
+	  pthread_exit(NULL);
+       }
       refresh_ap(&thread->info);
-      count = 0;
     }
   pthread_exit(NULL);
 }
